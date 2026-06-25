b28ed21d8edf7f8661cdd2b5dc0d5a854145992e
Ronaldo Firmansyah <55744190+Ronaldo-spec@users.noreply.github.com>
2026-06-25 23:20:12 +0700

    Stabilize OpenMusic API server startup

--- src/server.js
+++ src/server.js
@@ -1,65 +1,62 @@
 require('dotenv').config();
 
-const Hapi = require("@hapi/hapi");
-const songs = require("./api/songs");
-const albums = require("./api/albums");
-const SongsService = require('./services/postgres/SongsService');
+const Hapi = require('@hapi/hapi');
+const albums = require('./api/albums');
+const songs = require('./api/songs');
+const ClientError = require('./exceptions/ClientError');
 const AlbumsService = require('./services/postgres/AlbumsService');
-const SongsValidator = require('./validator/songs');
+const SongsService = require('./services/postgres/SongsService');
 const AlbumsValidator = require('./validator/albums');
-const ClientError = require("./exceptions/ClientError");
+const SongsValidator = require('./validator/songs');
 
 const init = async () => {
   const songsService = new SongsService();
-  const albumsService = new AlbumsService(); 
+  const albumsService = new AlbumsService();
+
   const server = Hapi.server({
-    port: process.env.PORT,
-    host: process.env.HOST,
+    port: process.env.PORT || 3000,
+    host: process.env.HOST || 'localhost',
     routes: {
       cors: {
         origin: ['*'],
       },
     },
   });
-  console.log(process.env.PORT);
- 
-  await server.register({
-    plugin: songs,
-    options: {
-      service: songsService,
-      validator: SongsValidator,
+
+  await server.register([
+    {
+      plugin: songs,
+      options: {
+        service: songsService,
+        validator: SongsValidator,
+      },
     },
+    {
+      plugin: albums,
+      options: {
+        service: albumsService,
+        validator: AlbumsValidator,
+      },
+    },
+  ]);
+
+  server.ext('onPreResponse', (request, h) => {
+    const { response } = request;
+
+    if (response instanceof ClientError) {
+      const newResponse = h.response({
+        status: 'fail',
+        message: response.message,
+      });
+      newResponse.code(response.statusCode);
+      return newResponse;
+    }
+
+    return h.continue;
   });
 
-  await server.register({
-    plugin: albums,
-    options: {
-      service: albumsService,
-      validator: AlbumsValidator,
-    },
-  });
-
-
-  // server.ext('onPreResponse', (request, h) => {
-  //   // mendapatkan konteks response dari request
-  //   const { response } = request;
-  
-  //   // penanganan client error secara internal.
-  //   if (response instanceof ClientError) {
-  //     const newResponse = h.response({
-  //       status: 'fail',
-  //       message: response.message,
-  //     });
-  //     newResponse.code(response.statusCode);
-  //     return newResponse;
-  //   }
-      
-  //   return h.continue;
-  // });
-
- 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
 };
- 
+
 init();
